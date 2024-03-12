6d06d286d377c180d096b9ac9b3f34279d6f1795
yen <oe327188@gmail.com>
2024-03-12 23:17:16 +0800

    Originally, the callback function was used to know the status of the MQAgent, but each additional state required a new callback function, which was not conducive to expansion, so dependency injection was used to change the original method.

---
+++ agent_observer.py
@@ -0,0 +1,9 @@
+class MQAgentObserver:
+    def onMessageProcessing(self,content:str):
+        pass
+    def onMessageReceived(self):
+        pass
+    def onConnected(self):
+        pass
+    def onDisconnected(self):
+        pass
