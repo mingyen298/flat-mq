ee8039dd85bcc8503facb2b3b70a9ecf4ad68258
yen <oe327188@gmail.com>
2024-03-17 23:01:09 +0800

    update: 1. In order to ensure that if the mqtt client needs to be replaced in the future, and the package is still available, IMqttClient is used as the client of MQAgent. Users can implement IMqttClient according to the mqtt client they want to use. 2.init is only used to declare and initialize data, and setup is responsible for the main initial setting work.

--- agent.py
+++ agent.py
@@ -1,8 +1,3 @@
-import asyncio
-import os
-import signal
-import time
-
 from .util import Util
 from gmqtt import Client as MQTTClient
 from .helper import MQPacketStatus
@@ -11,7 +6,13 @@ from .helper import Constants
 from .topic_management import TopicManagement
 from .packet_controller import MQTxController
 from .agent_observer import MQAgentObserver
+from .agent_config import MQAgentConfig
 # https://github.com/wialon/gmqtt
+
+from flat_mq_client.mqtt_factory import MqttFactory
+from flat_mq_client.i_client import IMqttClient
+from flat_mq_client.client_options_builder import MqttClientOptionsBuilder
+from flat_mq_client.client_options import MqttClientOptions
 
 
 class _MQBase:
@@ -19,6 +20,7 @@ class _MQBase:
         pass
 
     async def run(self):
+        self.setup()
         await self._start()
 
     async def release(self):
@@ -30,31 +32,62 @@ class _MQBase:
     async def _stop(self):
         return NotImplementedError
 
+    def setup(self):
+        return NotImplementedError
+
 
 class MQAgent(_MQBase):
-    def __init__(self, observer=MQAgentObserver()):
-        self._client = None
-        self.tp_management = TopicManagement()
-        self._tx_controller = MQTxController()
+    def __init__(self, observer=MQAgentObserver(), mq_config: MQAgentConfig = None):
+        self._client: IMqttClient = None
+        self._client_options: MqttClientOptions = None
+        self._mq_config = mq_config
+        self._client_id = ""
+        self._role_name = ""
+
+        self.tp_management: TopicManagement = None
+        self._tx_controller: MQTxController = None
         self._observer = observer
-        
+
+    def setup(self) -> None:
+        self.setupMQOptions()
+        self._tx_controller = MQTxController()
+        self.tp_management = TopicManagement()
+
+    def setupMQOptions(self) -> MqttClientOptions:
+        builder = MqttClientOptionsBuilder()
+        builder.withClientID("test123_user")
+        builder.withTcpServer(host=Constants().BrokerURL, port=1883)
+        builder.withCleanSession()
+        builder.withKeepAlive(sec=10)
+        return builder.build()
 
     def _subscribeTopic(self):
         self._client.subscribe(self.tp_management.getSubscribe(), qos=0)
 
-    async def send(self, packet: MQPacket) -> MQPacket:
+    def processContent(self, content: str) -> str:
+        pass
+
+    async def send(self, content: str) -> str:
         result: MQPacket = None
+        packet = MQPacket(content=content, sender_id=self._client_id,
+                          source=f"/role/{self._role_name}/{self._client_id}", status=MQPacketStatus.Rising)
+        result = await self._send(packet=packet, receive_role_topic=self.tp_management.getPublish())
+        if result is None:
+            return result
+        return result.response
+
+    async def _send(self, packet: MQPacket, receive_role_topic: str) -> MQPacket:
+        result: MQPacket = None
+
         try:
             if not isinstance(packet, MQPacket):
                 return result
 
-            packet.status = MQPacketStatus.Rising
             msg = MQPacketCovert.serialize(packet=packet)
 
             if self._client == None:
                 return result
-            self._client.publish(self.tp_management.getPublish(),
-                                 msg, qos=0)
+            self._client.publish(topic=receive_role_topic, msg=msg, qos=0)
             track = self._tx_controller.requestPacket(packet=packet)
             if track == None:
                 return result
@@ -65,7 +98,8 @@ class MQAgent(_MQBase):
             print("send error")
 
         return result
-    async def _response(self,packet:MQPacket) -> None:
+
+    async def _response(self, packet: MQPacket) -> None:
         try:
             if not isinstance(packet, MQPacket):
                 return None
@@ -81,17 +115,18 @@ class MQAgent(_MQBase):
             print("send error")
 
     async def _start(self):
-        self._client = MQTTClient("test123_user")
+        self._client: IMqttClient = MqttFactory().createGMqttClient()
+        self._client.on_msg = self.__onMessageReceived
         self._client.on_connect = self.__onConnected
-        self._client.on_message = self.__onMessageReceived
         self._client.on_disconnect = self.__onDisconnected
-        await self._client.connect(Constants().BrokerURL)
+        await self._client.startAsync(options=self._client_options)
+        await self._subscribeTopic()
 
     async def _stop(self):
-        await self._client.disconnect()
+        await self._client.stopAsync()
+        self._tx_controller = None
 
     def __onConnected(self, client, flags, rc, properties):
-        self._subscribeTopic()
         print('Connected')
         self._observer.onConnected()
 
@@ -106,10 +141,11 @@ class MQAgent(_MQBase):
 
             elif packet.status is MQPacketStatus.Rising:  # 剛送到待處理的訊息
                 packet.status = MQPacketStatus.Processing
-                self._observer.onMessageProcessing(content=packet.content)
+                response = self.processContent(content=packet.content)
+                packet.response = response
                 packet.status = MQPacketStatus.Falling
 
-                if packet.status is MQPacketStatus.Falling:
+                if packet.status is MQPacketStatus.Falling:  # 處理完了，把結果送回去
                     packet.status = MQPacketStatus.Finished
                     await self._response(packet=packet)
             self._observer.onMessageReceived()
