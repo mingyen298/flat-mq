669c9dfb5f94436e5259b48b25668706c8390301
yen <oe327188@gmail.com>
2024-03-18 23:51:46 +0800

    add: When constructing MQAgent, multiple data need to be filled in. If they are all written in __init__, this will increase the complexity of constructing MQAgent. Therefore, MQAgentConfig and MQAgentConfigBuilder are added to ensure the accuracy and readability of the data filled in by users.

---
+++ agent_config.py
@@ -0,0 +1,49 @@
+import json
+from typing import overload
+
+
+class MQAgentConfig:
+    def __init__(self) -> None:
+        self.agent_id: str = ""
+        self.listen_topic: str = ""
+        self.send_topics: dict = dict()
+        self.host: str = "0.0.0.0"
+        self.port:int = 1883
+
+
+class MQAgentConfigBuilder:
+    def __init__(self) -> None:
+        self._config = MQAgentConfig()
+        self._id = ""
+        self._role = ""
+        self._other_roles = dict()
+
+    def withSelfAgentID(self, role: str, id: str = "") -> None:
+        self._role = role
+        self._id = id
+
+    def withOtherAgentID(self, role: str, id: str = "") -> None:
+        if id == "":
+            self._other_roles[role] = f'role/{self._role}'
+        else:
+            self._other_roles[role] = f'role/{self._role}/{self._id}'
+
+    def withTcpServer(self,host:str , port:int)->None:
+        self._config.host = host
+        self._config.port = port
+
+    def build(self):
+
+        if id == "":
+            self._config.agent_id = f'{self._role}'
+            self._config.listen_topic = f'role/{self._role}'
+        else:
+            self._config.agent_id = f'{self._role}-{self._id}'
+            self._config.listen_topic = f'role/{self._role}/{self._id}'
+
+        for role, topic in self._other_roles.items():
+            self._config.listen_topic[role] = topic
+
+        return self._config
+
+
